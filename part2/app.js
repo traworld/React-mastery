2f95139bfa4c167107995e1442f67471b1d2c122
Sahil Singh <sahilmac145@gmail.com>
2024-03-03 12:23:15 +0530

    create h1 element

--- part2/app.js
+++ part2/app.js
@@ -1,3 +1,17 @@
-const news = React.createElement("h1", {}, "hello duniya")
+const news = React.createElement("h1", {
+    id:"head"
+}, "hello duniya!")
 const renderElement = ReactDOM.createRoot(document.getElementById("root"))
 renderElement.render(news);
+// the work of render is to take the react hading element and make it readable to browser and convert it into a ahtml and make it\
+
+
+// react element is nothing bu a simple javascript object
+
+// props are children plus attriburte that we pass
+
+// ("h1", {
+//    id:"head"
+// }, "hello duniya!")     // this is props
+
+// hello duniya is a children 
