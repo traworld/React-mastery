b56d5bd09cac7c12501b59a20da72e4d938421e4
Sahil Singh <sahilmac145@gmail.com>
2024-03-03 13:07:08 +0530

    sec part

--- part2/app.js
+++ part2/app.js
@@ -1,8 +1,8 @@
-const news = React.createElement("h1", {
-    id:"head"
-}, "hello duniya!")
-const renderElement = ReactDOM.createRoot(document.getElementById("root"))
-renderElement.render(news);
+// const news = React.createElement("h1", {
+//     id:"head"
+// }, "hello duniya!")
+// const renderElement = ReactDOM.createRoot(document.getElementById("root"))
+// renderElement.render(news);
 // the work of render is to take the react hading element and make it readable to browser and convert it into a ahtml and make it\
 
 
@@ -15,3 +15,17 @@ renderElement.render(news);
 // }, "hello duniya!")     // this is props
 
 // hello duniya is a children 
+
+
+// how to  create a nested element
+ 
+const newElement = React.createElement(
+    "div",
+    {id:"one"},
+    React.createElement(
+        "div",
+        {id:"two"}, 
+        React.createElement(
+            "h1",{}, "heyooo")));
+const rend = ReactDOM.createRoot(document.getElementById("root"));
+rend.render(newElement);
