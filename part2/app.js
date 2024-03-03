93c985147b01d7addb84224d11b92f248450efae
Sahil Singh <sahilmac145@gmail.com>
2024-03-03 13:24:38 +0530

    ne

--- part2/app.js
+++ part2/app.js
@@ -25,7 +25,14 @@ const newElement = React.createElement(
     React.createElement(
         "div",
         {id:"two"}, 
-        React.createElement(
-            "h1",{}, "heyooo")));
+        [React.createElement(
+            "h1",{}, "heyooo"),
+        React.createElement("h2",{}, "heyooo!!!!")
+        ]));
 const rend = ReactDOM.createRoot(document.getElementById("root"));
 rend.render(newElement);
+
+// to add more than one sibling to a child like a multiple h1  we can add a array and put it inside it
+// instead of parents we can add multiple child we can do the same as siblings
+
+// this is js we can use JSX that makes react easier to understand
